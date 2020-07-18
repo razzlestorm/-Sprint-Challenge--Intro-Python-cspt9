d16eaadcdca03e37044e144526b639668dfb303e
Jerimiah Willhite <willhite.jerimiah@gmail.com>
2020-07-17 19:10:54 -0700

    finish stretch goal with all tests passing

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -75,9 +75,12 @@ for c in cities:
 
 def cityreader_stretch(lat1, lon1, lat2, lon2, cities=[]):
   # within will hold the cities that fall within the specified region
-  within = []
 
+	within = [city for city in cities if
+ 			  (min(lat1, lat2) < city.lat < max(lat1, lat2))
+ 			  and (min(lon1, lon2) < city.lon < max(lon1, lon2))]
   # Go through each city and check to see if it falls within
   # the specified coordinates.
+	return within
 
-  return within
+cityreader_stretch(45,-100, 32,-120, cities)
